01ed7414dda79caec78af11e839a6e96e583569b
Paul Rodriguez <paul@prodriguez.dev>
2024-03-29 15:27:07 -0600

    Blog image hover effect

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,5 +1,125 @@
-import React from "react";
+"use client";
 
-export default function ContentList() {
-  return <div>ContentList</div>;
+import React, { useEffect, useRef, useState } from "react";
+import { Content, asImageSrc, isFilled } from "@prismicio/client";
+import { MdArrowOutward } from "react-icons/md";
+import Link from "next/link";
+import gsap from "gsap";
+
+type ContentListProps = {
+  items: Content.BlogPostDocument[] | Content.ProjectDocument[];
+  contentType: Content.ContentIndexSlice["primary"]["content_type"];
+  fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
+  viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
+};
+
+export default function ContentList({
+  items,
+  contentType,
+  fallbackItemImage,
+  viewMoreText = "Read More",
+}: ContentListProps) {
+  const component = useRef(null);
+  const revealRef = useRef(null);
+  const [currentItem, setCurrentItem] = useState<null | number>(null);
+
+  const urlPrefix = contentType === "Blog" ? "/blog" : "/project";
+
+  const lastMousePos = useRef({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const mousePos = { x: e.clientX, y: e.clientY + window.scrollY };
+
+      // Calculate speed and direction
+      const speed = Math.sqrt(Math.pow(mousePos.x - lastMousePos.current.x, 2));
+
+      let ctx = gsap.context(() => {
+        if (currentItem !== null) {
+          const maxY = window.scrollY + window.innerHeight - 350;
+          const maxX = window.innerWidth - 250;
+
+          gsap.to(revealRef.current, {
+            x: gsap.utils.clamp(0, maxX, mousePos.x - 110),
+            y: gsap.utils.clamp(0, maxY, mousePos.y - 160),
+            rotation: speed * (mousePos.x > lastMousePos.current.x ? 1 : -1),
+            ease: "back.out(2)",
+            duration: 1.3,
+          });
+        }
+      });
+    };
+  });
+
+  const contentImages = items.map((item) => {
+    const image = isFilled.image(item.data.hover_image)
+      ? item.data.hover_image
+      : fallbackItemImage;
+
+    return asImageSrc(image, {
+      fit: "crop",
+      w: 220,
+      h: 320,
+      exp: -10,
+    });
+  });
+
+  const onMouseEnter = (index: number) => {
+    setCurrentItem(index);
+  };
+
+  const onMouseLeave = () => {
+    setCurrentItem(null);
+  };
+
+  return (
+    <div>
+      <ul
+        className="grid border-b border-b-slate-100"
+        onMouseLeave={onMouseLeave}
+      >
+        {items.map((item, index) => (
+          <>
+            {isFilled.keyText(item.data.title) && (
+              <li
+                key={index}
+                className="list-item opacity-0f"
+                onMouseEnter={() => onMouseEnter(index)}
+              >
+                <Link
+                  href={urlPrefix + "/" + item.uid}
+                  className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
+                  aria-label={item.data.title}
+                >
+                  <div className="flex flex-col">
+                    <span className="text-3xl font-bold">
+                      {item.data.title}
+                    </span>
+                    <div className="flex gap-3 text-yellow-400 text-lg font-bold">
+                      {item.tags.map((tag, index) => (
+                        <span key={index}>{tag}</span>
+                      ))}
+                    </div>
+                  </div>
+                  <span className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
+                    {viewMoreText} <MdArrowOutward />
+                  </span>
+                </Link>
+              </li>
+            )}
+          </>
+        ))}
+      </ul>
+
+      {/* Hover Element */}
+      <div
+        className="hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-over bg-center opacity-0f transition-[background] duration-300"
+        style={{
+          backgroundImage:
+            currentItem != null ? `url(${contentImages[currentItem]})` : "",
+        }}
+        ref={revealRef}
+      ></div>
+    </div>
+  );
 }
--- src/slices/ContentIndex/index.tsx
+++ src/slices/ContentIndex/index.tsx
@@ -3,6 +3,7 @@ import Heading from "@/components/Heading";
 import { Content, isFilled } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import ContentList from "./ContentList";
+import { createClient } from "@/prismicio";
 
 /**
  * Props for `ContentIndex`.
@@ -12,7 +13,17 @@ export type ContentIndexProps = SliceComponentProps<Content.ContentIndexSlice>;
 /**
  * Component for "ContentIndex" Slices.
  */
-const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
+const ContentIndex = async ({
+  slice,
+}: ContentIndexProps): Promise<JSX.Element> => {
+  const client = createClient();
+  const blogPosts = await client.getAllByType("blog_post");
+  const projects = await client.getAllByType("project");
+
+  const contentType = slice.primary.content_type || "Blog";
+
+  const items = contentType === "Blog" ? blogPosts : projects;
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -26,7 +37,12 @@ const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
           <PrismicRichText field={slice.primary.description} />
         </div>
       )}
-      <ContentList />
+      <ContentList
+        items={items}
+        contentType={contentType}
+        viewMoreText={slice.primary.view_more_text}
+        fallbackItemImage={slice.primary.fallback_item_image}
+      />
     </Bounded>
   );
 };

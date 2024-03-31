b5040869343ba2bef8fb7da56b4339fb980e164d
Paul Rodriguez <paul@prodriguez.dev>
2024-03-30 18:35:12 -0600

    content index update

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
-import { Content, asImageSrc, isFilled } from "@prismicio/client";
-import { MdArrowOutward } from "react-icons/md";
-import Link from "next/link";
-import gsap from "gsap";
+import React, { useRef, useState, useEffect } from "react";
+import { asImageSrc, isFilled } from "@prismicio/client";
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { MdArrowOutward } from "react-icons/md";
+import { Content } from "@prismicio/client";
 
 gsap.registerPlugin(ScrollTrigger);
 
 type ContentListProps = {
   items: Content.BlogPostDocument[] | Content.ProjectDocument[];
-  contentType: Content.ContentIndexSlice["primary"]["content_type"];
-  fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
-  viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
+  contentType: Content.BlogPostIndexSlice["primary"]["content_type"];
+  fallbackItemImage: Content.BlogPostIndexSlice["primary"]["fallback_item_image"];
+  viewMoreText: Content.BlogPostIndexSlice["primary"]["view_more_text"];
 };
 
 export default function ContentList({
@@ -23,13 +23,14 @@ export default function ContentList({
   viewMoreText = "Read More",
 }: ContentListProps) {
   const component = useRef(null);
-  const revealRef = useRef(null);
   const itemsRef = useRef<Array<HTMLLIElement | null>>([]);
-  const [currentItem, setCurrentItem] = useState<null | number>(null);
 
+  const revealRef = useRef(null);
+  const [currentItem, setCurrentItem] = useState<null | number>(null);
+  const [hovering, setHovering] = useState(false);
   const lastMousePos = useRef({ x: 0, y: 0 });
 
-  const urlPrefix = contentType === "Blog" ? "/blog" : "/project";
+  const urlPrefix = contentType === "Blogs" ? "/blog" : "/project";
 
   useEffect(() => {
     // Animate list-items in with a stagger
@@ -62,13 +63,14 @@ export default function ContentList({
   }, []);
 
   useEffect(() => {
+    // Mouse move event listener
     const handleMouseMove = (e: MouseEvent) => {
       const mousePos = { x: e.clientX, y: e.clientY + window.scrollY };
-
       // Calculate speed and direction
       const speed = Math.sqrt(Math.pow(mousePos.x - lastMousePos.current.x, 2));
 
       let ctx = gsap.context(() => {
+        // Animate the image holder
         if (currentItem !== null) {
           const maxY = window.scrollY + window.innerHeight - 350;
           const maxX = window.innerWidth - 250;
@@ -76,15 +78,19 @@ export default function ContentList({
           gsap.to(revealRef.current, {
             x: gsap.utils.clamp(0, maxX, mousePos.x - 110),
             y: gsap.utils.clamp(0, maxY, mousePos.y - 160),
-            rotation: speed * (mousePos.x > lastMousePos.current.x ? 1 : -1),
+            rotation: speed * (mousePos.x > lastMousePos.current.x ? 1 : -1), // Apply rotation based on speed and direction
             ease: "back.out(2)",
             duration: 1.3,
-            opacity: 1,
+          });
+          gsap.to(revealRef.current, {
+            opacity: hovering ? 1 : 0,
+            visibility: "visible",
+            ease: "power3.out",
+            duration: 0.4,
           });
         }
-
         lastMousePos.current = mousePos;
-        return () => ctx.revert();
+        return () => ctx.revert(); // cleanup!
       }, component);
     };
 
@@ -93,20 +99,22 @@ export default function ContentList({
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
-  });
+  }, [hovering, currentItem]);
 
-  const contentImages = [
-    ...items,
-    ...items,
-    ...items,
-    ...items,
-    ...items,
-    ...items,
-  ].map((item) => {
-    const image = isFilled.image(item.data.hover_image)
-      ? item.data.hover_image
+  const onMouseEnter = (index: number) => {
+    setCurrentItem(index);
+    if (!hovering) setHovering(true);
+  };
+
+  const onMouseLeave = () => {
+    setHovering(false);
+    setCurrentItem(null);
+  };
+
+  const contentImages = items.map((item) => {
+    const image = isFilled.image(item.data.image)
+      ? item.data.image
       : fallbackItemImage;
-
     return asImageSrc(image, {
       fit: "crop",
       w: 220,
@@ -115,65 +123,61 @@ export default function ContentList({
     });
   });
 
-  const onMouseEnter = (index: number) => {
-    setCurrentItem(index);
-  };
-
-  const onMouseLeave = () => {
-    setCurrentItem(null);
-  };
+  // Preload images
+  useEffect(() => {
+    contentImages.forEach((url) => {
+      if (!url) return;
+      const img = new Image();
+      img.src = url;
+    });
+  }, [contentImages]);
 
   return (
-    <div>
+    <>
       <ul
+        ref={component}
         className="grid border-b border-b-slate-100"
         onMouseLeave={onMouseLeave}
       >
-        {[...items, ...items, ...items, ...items, ...items, ...items].map(
-          (item, index) => (
-            <>
-              {isFilled.keyText(item.data.title) && (
-                <li
-                  key={index}
-                  className="list-item opacity-0f"
-                  onMouseEnter={() => onMouseEnter(index)}
-                  ref={(el) => (itemsRef.current[index] = el)}
-                >
-                  <Link
-                    href={urlPrefix + "/" + item.uid}
-                    className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
-                    aria-label={item.data.title}
-                  >
-                    <div className="flex flex-col">
-                      <span className="text-3xl font-bold">
-                        {item.data.title}
-                      </span>
-                      <div className="flex gap-3 text-yellow-400 text-lg font-bold">
-                        {item.tags.map((tag, index) => (
-                          <span key={index}>{tag}</span>
-                        ))}
-                      </div>
-                    </div>
-                    <span className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
-                      {viewMoreText} <MdArrowOutward />
+        {items.map((post, index) => (
+          <li
+            key={index}
+            ref={(el) => (itemsRef.current[index] = el)}
+            onMouseEnter={() => onMouseEnter(index)}
+            className="list-item opacity-0"
+          >
+            <a
+              href={`${urlPrefix}/${post.uid}`}
+              className="flex flex-col justify-between border-t border-t-slate-100 py-10  text-slate-200 md:flex-row "
+              aria-label={post.data.title || ""}
+            >
+              <div className="flex flex-col">
+                <span className="text-3xl font-bold">{post.data.title}</span>
+                <div className="flex gap-3 text-yellow-400">
+                  {post.tags.map((tag, index) => (
+                    <span key={index} className="text-lg font-bold">
+                      {tag}
                     </span>
-                  </Link>
-                </li>
-              )}
-            </>
-          )
-        )}
-      </ul>
+                  ))}
+                </div>
+              </div>
+              <span className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
+                {viewMoreText} <MdArrowOutward />
+              </span>
+            </a>
+          </li>
+        ))}
 
-      {/* Hover Element */}
-      <div
-        className="hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-over bg-center opacity-0f transition-[background] duration-300"
-        style={{
-          backgroundImage:
-            currentItem != null ? `url(${contentImages[currentItem]})` : "",
-        }}
-        ref={revealRef}
-      ></div>
-    </div>
+        {/* Hover element */}
+        <div
+          className="hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-cover bg-center opacity-0 transition-[background] duration-300"
+          style={{
+            backgroundImage:
+              currentItem !== null ? `url(${contentImages[currentItem]})` : "",
+          }}
+          ref={revealRef}
+        ></div>
+      </ul>
+    </>
   );
 }

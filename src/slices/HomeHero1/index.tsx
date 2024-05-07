1000e426fdcc9b521c1fdac802d416e7a6003535
Paul Rodriguez <paul@prodriguez.dev>
2024-05-06 23:41:05 -0600

    update homehero

--- src/slices/HomeHero1/index.tsx
+++ src/slices/HomeHero1/index.tsx
@@ -1,8 +1,8 @@
 import Bounded from "@/components/Bounded";
 import { Content, isFilled } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
+import gsap from "gsap";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
-import image from "next/image";
 
 /**
  * Props for `HomeHero1`.
@@ -20,9 +20,10 @@ const HomeHero1 = ({ slice }: HomeHero1Props): JSX.Element => {
       className="home-hero-1 -mt-32 bg-[url('/bg/paul-stage-bg-blue-2.svg')] bg-cover bg-no-repeat object-cover pt-24"
     >
       <Bounded>
-
-        <div className="flex mx-36 mt-48 mb-36">
-          {isFilled.keyText(slice.primary.description) && <h2>{slice.primary.description}</h2>}
+        <div className="mx-36 mb-36 mt-48 flex">
+          {isFilled.keyText(slice.primary.description) && (
+            <h2>{slice.primary.description}</h2>
+          )}
           <PrismicNextImage
             field={slice.primary.avatar}
             className="object-fit w-36"

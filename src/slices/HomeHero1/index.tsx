2cc68e628334236cdd5f9a1d375608ae0365fa8c
prodriguez-dev <paul@prodriguez.dev>
2024-05-22 02:13:37 -0600

    home hero scales now

--- src/slices/HomeHero1/index.tsx
+++ src/slices/HomeHero1/index.tsx
@@ -20,6 +20,7 @@ export type HomeHero1Props = SliceComponentProps<Content.HomeHero1Slice>;
  * Component for "HomeHero1" Slices.
  */
 const HomeHero1 = ({ slice }: HomeHero1Props): JSX.Element => {
+  const component = useRef(null);
   const imageRef = useRef(null);
   const textRef = useRef(null);
   const bgBackgroundRef = useRef(null);
@@ -28,7 +29,7 @@ const HomeHero1 = ({ slice }: HomeHero1Props): JSX.Element => {
 
   useEffect(() => {
     const handleLoad = () => {
-      console.log("Page fully loaded");
+      // console.log("Page fully loaded");
       setPageLoaded(true);
     };
 
@@ -43,64 +44,68 @@ const HomeHero1 = ({ slice }: HomeHero1Props): JSX.Element => {
 
   useEffect(() => {
     if (isPageLoaded) {
-      console.log("Initializing animations");
-      const masterTimeline = gsap.timeline();
+      // console.log("Initializing animations");
+      let ctx = gsap.context(() => {
+        const masterTimeline = gsap.timeline();
 
-      // Fade out the loading text first
-      if (loadingTextRef.current) {
-        masterTimeline.to(loadingTextRef.current, {
-          opacity: 0,
-          duration: 0.5,
-          ease: "Power1.easeOut",
-          onComplete: () => {
-            gsap.set(loadingTextRef.current, { display: "none" });
-          },
-        });
-      }
-
-      // Background animation
-      if (bgBackgroundRef.current) {
-        masterTimeline.fromTo(
-          bgBackgroundRef.current,
-          { opacity: 0, top: 800 },
-          { opacity: 1, top: 0, duration: 0.7, ease: "Power3.easeIn" },
-        );
-      }
-
-      // Image animation
-      masterTimeline.fromTo(
-        imageRef.current,
-        {
-          opacity: 0,
-          scale: 0.5,
-        },
-        {
-          duration: 1.0,
-          opacity: 1,
-          scale: 1,
-          ease: "Power3.easeOut",
-        },
-      );
-
-      // Text animation
-      if (textRef.current) {
-        const split = new SplitText(textRef.current, { type: "words" });
-        const textTimeline = gsap.timeline();
-        split.words.forEach((word, index) => {
-          textTimeline.from(
-            word,
-            {
-              duration: 0.8,
-              y: 50,
-              opacity: 0,
-              ease: "Power3.easeOut",
-              force3D: true,
+        // Fade out the loading text first
+        if (loadingTextRef.current) {
+          masterTimeline.to(loadingTextRef.current, {
+            opacity: 0,
+            duration: 0.5,
+            ease: "Power1.easeOut",
+            onComplete: () => {
+              gsap.set(loadingTextRef.current, { display: "none" });
             },
-            index * 0.1,
+          });
+        }
+
+        // Background animation
+        if (bgBackgroundRef.current) {
+          masterTimeline.fromTo(
+            bgBackgroundRef.current,
+            { opacity: 0 },
+            { opacity: 1, duration: 0.5, ease: "Power3.easeIn" },
           );
-        });
-        masterTimeline.add(textTimeline, "+=0.5");
-      }
+        }
+
+        // Image animation
+        masterTimeline.fromTo(
+          imageRef.current,
+          {
+            opacity: 0,
+            scale: 0.5,
+          },
+          {
+            duration: 1.0,
+            opacity: 1,
+            scale: 1,
+            ease: "Power3.easeOut",
+          },
+        );
+
+        // Text animation
+        if (textRef.current) {
+          const split = new SplitText(textRef.current, { type: "words" });
+          const textTimeline = gsap.timeline();
+          split.words.forEach((word, index) => {
+            textTimeline.from(
+              word,
+              {
+                duration: 0.8,
+                y: 50,
+                opacity: 0,
+                ease: "Power3.easeOut",
+                force3D: true,
+              },
+              index * 0.1,
+            );
+          });
+          masterTimeline.add(textTimeline, "+=0.5");
+        }
+      }, component);
+
+      return () => ctx.revert();
     }
   }, [isPageLoaded]);
 
@@ -112,14 +117,13 @@ const HomeHero1 = ({ slice }: HomeHero1Props): JSX.Element => {
     >
       <div
         ref={loadingTextRef}
-        className="loading-text"
+        className={clsx("loading-text", s.loading_text)}
         style={{
           opacity: 1,
           position: "absolute",
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
-          fontSize: "2rem",
           color: "white",
         }}
       >
@@ -135,25 +139,27 @@ const HomeHero1 = ({ slice }: HomeHero1Props): JSX.Element => {
           />
         </div>
       )}
-      <div className={clsx(s.hero_bounded, !isPageLoaded && "opacity-0")}>
-        {isFilled.richText(slice.primary.description) && (
-          <div ref={textRef} className={clsx(s.hero_text, "text-blue-950")}>
-            <PrismicRichText field={slice.primary.description} />
-          </div>
-        )}
-        {isFilled.image(slice.primary.avatar) && (
-          <div ref={imageRef}>
-            <PrismicNextImage
-              field={slice.primary.avatar}
-              className={s.hero_image}
-              imgixParams={{ q: 90 }}
-              placeholder="empty"
-              priority
-            />
-          </div>
-        )}
+      <div className={clsx(s.hero_bounded_wrapper, !isPageLoaded && "opacity-0")}>
+        <div className={s.hero_bounded}>
+          {isFilled.richText(slice.primary.description) && (
+            <div ref={textRef} className={clsx(s.hero_text, "text-blue-950")}>
+              <PrismicRichText field={slice.primary.description} />
+            </div>
+          )}
+          {isFilled.image(slice.primary.avatar) && (
+            <div ref={imageRef}>
+              <PrismicNextImage
+                field={slice.primary.avatar}
+                className={s.hero_image}
+                imgixParams={{ q: 90 }}
+                placeholder="empty"
+                priority
+              />
+            </div>
+          )}
+        </div>
       </div>
-    </section>
+    </section >
   );
 };
 
